724bdfe6e33715f94201ef15faf95a342ff477f1
Hhhr <Hhhr1214@gmail.com>
2021-06-25 14:09:57 +0800

    首页轮播图

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,10 @@
 <template>
-	<view>
-	首页
+	<view class="home">
+		<swiper indicator-dots circular>
+			<swiper-item v-for="item in swipers" :key="item.id">
+				<image :src="item.image_src"></image>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,18 +12,36 @@
 	export default {
 		data() {
 			return {
-
+				swipers: []
 			}
 		},
 		onLoad() {
-
+			this.getSwipers()
 		},
 		methods: {
-
+			//轮播图
+			async getSwipers() {
+				console.log('获取轮播图数据')
+				const res = await this.$myRuquest({
+					url: '/api/public/v1/home/swiperdata'
+				})
+				console.log(res.data.message)
+				this.swipers = res.data.message
+				console.log(this.swipers[0].image_src)
+			}
 		}
 	}
 </script>
 
-<style scoped>
-
+<style lang="less">
+.home{
+	swiper{
+		width: 750rpx;
+		height: 380rpx;
+		image{
+			height: 100%;
+			width: 100%;
+		}
+	}
+}
 </style>
